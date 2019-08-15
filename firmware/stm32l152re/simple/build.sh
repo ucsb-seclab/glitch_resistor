7fb86cae2c63343fde2257a236d1497c6e70f9fe
Chad Spensky <cspensky@gmail.com>
2019-08-15 10:19:14 -0400

    Adding both defenses back to firmware build

--- firmware/stm32l152re/simple/build.sh
+++ firmware/stm32l152re/simple/build.sh
@@ -4,6 +4,6 @@
   
 export LLVM_COMPILER=clang
 export CC=wllvm
+export CLANG_FLAGS+="-Xclang -load -Xclang ../../../instrumenter/build/DelayInjector/libDelayInjector.so "
 export CLANG_FLAGS+="-Xclang -load -Xclang ../../../instrumenter/build/BranchProtector/libBranchProtector.so "
-#export CLANG_FLAGS+="-Xclang -load -Xclang ../../../instrumenter/build/DelayInjector/libDelayInjector.so "
 CC=clang make
