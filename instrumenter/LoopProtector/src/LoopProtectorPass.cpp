41f2ac6ffe1e53efa727a0db8a176d72345ae941
Aravind Machiry <machiry@cs.ucsb.edu>
2019-08-22 10:50:47 -0400

    Finishing up loop protector pass

--- instrumenter/LoopProtector/src/LoopProtectorPass.cpp
+++ instrumenter/LoopProtector/src/LoopProtectorPass.cpp
@@ -144,9 +144,9 @@ public:
   }
 
   /***
-   *
-   * @param F
-   * @return
+   * Create a new local integer variable at the entry of the function.
+   * @param F Function in which the local variable needs to be created.
+   * @return Pointer to the created local variable.
    */
   Value* createNewLocalVariable(Function &F)
   {
@@ -156,20 +156,40 @@ public:
     return newAlloca;
   }
 
-  bool getEnteringBBs(Loop* currLoop, std::set<BasicBlock*> &enteringBBs) {
+  /***
+   *  Get all the basic-blocks that are predecessors to the loops header
+   *  i.e., these are the basic-blocks immediately precede the loop.
+   * @param currLoop Target loop.
+   * @param enteringBBs Set where the pointers to the basic blocks should be stored.
+   * @return true if there are some basic blocks
+   */
+  bool getEnteringBBs(Loop* currLoop, std::set<BasicBlock*> &enteringBBs)
+  {
     BasicBlock *hdr = currLoop->getHeader();
-    for (auto it = pred_begin(hdr), et = pred_end(hdr); it != et; ++it) {
+    for (auto it = pred_begin(hdr), et = pred_end(hdr); it != et; ++it)
+    {
       BasicBlock* predecessor = *it;
-      if(!currLoop->contains(predecessor)) {
+      if (!currLoop->contains(predecessor))
+      {
         enteringBBs.insert(predecessor);
       }
     }
     return !enteringBBs.empty();
   }
 
-  void storeValue(unsigned storeCons, Value *dstPtr, BasicBlock *dstBB) {
+  /***
+   * Create a store instruction to store given constant into the
+   * provided variable.
+   *
+   * @param storeCons Constant value to be stored.
+   * @param dstPtr Pointer to the local variable.
+   * @param dstBB Basic block where store instruction should be created
+   */
+  void storeValue(unsigned storeCons, Value *dstPtr, BasicBlock *dstBB)
+  {
     IRBuilder<> builder(&(*dstBB->getFirstInsertionPt()));
-    if(dstBB->getInstList().size() > 1) {
+    if (dstBB->getInstList().size() > 1)
+    {
       auto termIt = dstBB->getTerminator()->getIterator();
       termIt--;
       builder.SetInsertPoint(&(*termIt));
@@ -178,24 +198,36 @@ public:
     builder.CreateStore(cons, dstPtr, true);
   }
 
-  void resetValue(Value* loopVar, std::set<BasicBlock*> &targetBBs) {
+  /***
+   *  Reset the value of the provided loop variable with 0
+   *
+   * @param loopVar pointer to the local variable which needs to be reset.
+   * @param targetBBs Blocks where the value should be reset.
+   */
+  void resetValue(Value* loopVar, std::set<BasicBlock*> &targetBBs)
+  {
     // in each of the basic block..store 0
-    for(auto currBB: targetBBs) {
+    for (auto currBB: targetBBs)
+    {
       storeValue(0, loopVar, currBB);
     }
   }
 
   /***
+   *  Get the correspondence between the loop exit basic blocks
+   *  and corresponding in loop exiting basic blocks.
    *
-   * @param lo
-   * @param co
-   * @return
+   * @param lo Loop object.
+   * @param co Map that contains the correspondence between exit and corresponding exiting basic blocks.
+   * @return true if there are non-zero exit basic blocks.
    */
-  bool getExitBBCorrespondence(Loop *lo, std::map<BasicBlock*, std::set<BasicBlock*>> &co) {
+  bool getExitBBCorrespondence(Loop *lo, std::map<BasicBlock*, std::set<BasicBlock*>> &co)
+  {
     SmallVector<Loop::Edge, 32> exitEdges;
     exitEdges.clear();
     lo->getExitEdges(exitEdges);
-    for(auto &currEdge: exitEdges) {
+    for (auto &currEdge: exitEdges)
+    {
       BasicBlock *outBB = const_cast<BasicBlock*>(currEdge.second);
       BasicBlock *inBB = const_cast<BasicBlock*>(currEdge.first);
       co[outBB].insert(inBB);
@@ -204,33 +236,37 @@ public:
   }
 
 
-  BasicBlock* getLoopCheckFailedBB(Loop *LI) {
-    BasicBlock *loopCheckFailed = BasicBlock::Create(LI->getHeader()->getContext(), "loopCheckFail");
-    IRBuilder<> builder(loopCheckFailed);
-    // create a call to glitch detected function.
-    builder.CreateCall(getGRFunction(*(LI->getHeader()->getModule())));
-    SmallVector<BasicBlock*, 32> exitBBs;
-    exitBBs.clear();
-    LI->getExitBlocks(exitBBs);
-    builder.CreateBr(exitBBs[0]);
-    loopCheckFailed->insertInto(LI->getHeader()->getParent(), exitBBs[0]);
-    return loopCheckFailed;
-
-  }
-
-
+  /***
+   *  Protect the provided loops by editing the loop exit basic blocks
+   *  by checking that the loop exited from expected basic blocks.
+   *  i.e.,
+   *   if loopv == val1 || loopv == val2 .. successful
+   *   else fail
+   *
+   * @param exitBBCorrespondence Map between loop exit basic block and
+   *                             corresponding in loop basic blocks.
+   * @param exitingBBCodes Map of in loop basic block and corresponding
+   *                       numerical codes.
+   * @param loopVariable Variable which contains the value corresponding to the
+   *                     exit of the loop.
+   * @return True, if any instrumentation is done else false.
+   */
   bool protectLoopExits(std::map<BasicBlock*, std::set<BasicBlock*>> &exitBBCorrespondence,
                         std::map<BasicBlock*, unsigned> &exitingBBCodes,
-                        Value *loopVariable) {
+                        Value *loopVariable)
+                        {
 
     LLVMContext &C = loopVariable->getContext();
-    for(auto &exitBBCoL:exitBBCorrespondence) {
+    for (auto &exitBBCoL:exitBBCorrespondence)
+    {
       BasicBlock *exitingBB = exitBBCoL.first;
       std::set<BasicBlock*> &inLoopBBs = exitBBCoL.second;
 
+      // create a fall through block for the exiting bb
       BasicBlock *exitFallThrough = BasicBlock::Create(exitingBB->getContext(), "exitFallThrough");
       exitFallThrough->insertInto(exitingBB->getParent(), exitingBB);
       IRBuilder<> builder(exitFallThrough);
+      // jump to the exiting bb
       builder.CreateBr(exitingBB);
 
       Value* loopLoadVal = nullptr;
@@ -238,20 +274,27 @@ public:
       BasicBlock *prevBB = nullptr;
       Value *prevBBICmd = nullptr;
 
-      for(auto *inLoopBB: inLoopBBs) {
-        unsigned currNum = exitingBBCodes[inLoopBB];
+      // for each of the in loop basic blocks, create a comparision instruction
+      for (auto *inLoopBB: inLoopBBs)
+      {
+        unsigned bbUniqueNumber = exitingBBCodes[inLoopBB];
         BasicBlock *cmpBB = BasicBlock::Create(C, "checkVal");
         cmpBB->insertInto(exitingBB->getParent(), exitFallThrough);
         IRBuilder<> inCmdBuilder(cmpBB);
-        if(firstCheck == nullptr) {
+        // is this first bb in the chain of comparisons
+        if (firstCheck == nullptr)
+        {
           loopLoadVal = inCmdBuilder.CreateLoad(loopVariable);
           firstCheck = cmpBB;
         }
         assert(loopLoadVal != nullptr && "Load value cannot be null.");
-        Value *cons = ConstantInt::get(IntegerType::getInt32Ty(C), currNum);
+        Value *cons = ConstantInt::get(IntegerType::getInt32Ty(C), bbUniqueNumber);
+        // create a comparision instruction.
         Value *icmpEq = inCmdBuilder.CreateICmpEQ(loopLoadVal, cons);
 
-        if(prevBB != nullptr) {
+        if (prevBB != nullptr)
+        {
+          // link to the previous basic-block
           inCmdBuilder.SetInsertPoint(prevBB);
           inCmdBuilder.CreateCondBr(prevBBICmd, exitFallThrough, cmpBB);
         }
@@ -260,28 +303,38 @@ public:
         prevBBICmd = icmpEq;
       }
 
-      BasicBlock *terminatingBB = BasicBlock::Create(exitingBB->getContext(), "terminatingBB");
+      // create loop check failed BB
+      BasicBlock *terminatingBB = BasicBlock::Create(exitingBB->getContext(), "LoopCheckFailed");
       terminatingBB->insertInto(exitingBB->getParent(), exitFallThrough);
       builder.SetInsertPoint(terminatingBB);
       builder.CreateCall(getGRFunction(*(exitingBB->getModule())));
       builder.CreateBr(exitFallThrough);
+
+      // link the last BB to the check failed BB
       builder.SetInsertPoint(prevBB);
       builder.CreateCondBr(prevBBICmd, exitFallThrough, terminatingBB);
 
 
       // update the targets of all BBs in inLoopBBs from exitingBB to firstCheck
-      for(auto currILBB: inLoopBBs) {
+      // instead of jumping to exit, jump to the checking chain.
+      for (auto currILBB: inLoopBBs)
+      {
         Instruction* termInst = currILBB->getTerminator();
         termInst->replaceUsesOfWith(exitingBB, firstCheck);
       }
 
       // finally update the PHI instructions in exitingBB that refer to one of the
-      for(auto &currInstr: *exitingBB) {
+      // inloop BB with exitFallThrough BB
+      for (auto &currInstr: *exitingBB)
+      {
         Instruction *currInstrPtr = &currInstr;
-        if(PHINode *phiInstr = dyn_cast<PHINode>(currInstrPtr)) {
-          for(auto targetBB: inLoopBBs) {
+        if (PHINode *phiInstr = dyn_cast<PHINode>(currInstrPtr))
+        {
+          for (auto targetBB: inLoopBBs)
+          {
             int bbIndx = phiInstr->getBasicBlockIndex(targetBB);
-            if (bbIndx >= 0) {
+            if (bbIndx >= 0)
+            {
               Value *targetValue = phiInstr->getIncomingValue(bbIndx);
               phiInstr->removeIncomingValue(bbIndx);
               phiInstr->addIncoming(targetValue, exitFallThrough);
@@ -306,7 +359,6 @@ public:
 
     LoopInfo &LI = getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
 
-    // errs() << TAG << LI << "\n";
     if (isFunctionSafeToModify(&F))
     {
       // get all the loops in the function.
@@ -337,7 +389,8 @@ public:
         unsigned solIdx = 0;
 
         // assign one code for each of the exiting BB
-        for(auto currExitBB: exitBBs) {
+        for (auto currExitBB: exitBBs)
+        {
           exitingBBCodes[currExitBB] = solomonCodes[solIdx];
           // store the corresponding value into the loop local var.
           storeValue(solomonCodes[solIdx], loopVar, currExitBB);
