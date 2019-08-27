3a033243289132cd0c645962d156574649f68781
Chad Spensky <cspensky@gmail.com>
2019-08-27 16:40:31 -0400

    Adding firmware that we plan to glitch

---
+++ firmware/stm32/hollow/build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+../../../build_passes.sh
+
+export LLVM_CC=clang
+export CPATH=../../../runtime//gcc-arm-none-eabi-8-2019-q3-update/arm-none-eabi/include/  
+#export LLVM_CC=clang
+#export CLANG_FLAGS+=" -Xclang -load -Xclang ../../../instrumenter/build/DelayInjector/libDelayInjector.so"
+export CLANG_FLAGS+=" -Xclang -load -Xclang ../../../instrumenter/build/BranchProtector/libBranchProtector.so"
+#export CLANG_FLAGS+=" -Xclang -load -Xclang ../../../instrumenter/build/LoopProtector/libLoopProtector.so"
+#export CLANG_FLAGS+=" -Xclang -load -Xclang ../../../instrumenter/build/IntegrityProtector/libIntegrityProtector.so"
+make
