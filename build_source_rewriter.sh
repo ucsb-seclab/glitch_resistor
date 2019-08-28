cbf6bd74cbc460120d85a6bc10c3f9ea75b1504b
Chad Spensky <cspensky@gmail.com>
2019-08-28 10:42:06 -0400

    Updating source rewriter build script and moving examples

--- build_source_rewriter.sh
+++ build_source_rewriter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp -r gp_source_rewriter llvm-8.0.0.src/tools/clang/tools
+cp -r gp_source_rewriter llvm-8.0.0.src/tools/clang/tools/
 
 if ! grep -q "gp_source_rewriter" llvm-8.0.0.src/tools/clang/tools/CMakeLists.txt; then
 	sed -i '' '/add_clang_subdirectory(clang-format)/ a\
@@ -9,5 +9,8 @@ add_clang_subdirectory(gp_source_rewriter)\
 fi
 
 cd llvm-8.0.0.obj
-cmake -DCMAKE_BUILD_TYPE=Debug ../llvm-8.0.0.src
-make gp-source-rewriter
+#cmake -DCMAKE_BUILD_TYPE=Debug ../llvm-8.0.0.src
+#make gp-source-rewriter
+
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="RISCV" ../llvm-8.0.0.src 
+cmake --build . --target gp-source-rewriter
