5c8b607f95cb236148dd66db9b9bd11c2d14a3a2
Chad Spensky <cspensky@gmail.com>
2019-08-29 14:18:59 -0400

    Unprotecting our instrumentation functions, this doesn't actually make sense

--- firmware/stm32/test_firmware/Src/delayfunc.c
+++ firmware/stm32/test_firmware/Src/delayfunc.c
@@ -1,4 +1,4 @@
-#include <stdlib.h> 
+#include <stdlib.h>
 // void gpdelay(void) {
 //    // make sure that you initialize
 //    // srand before this.
@@ -13,34 +13,35 @@
 // }
 unsigned int delay_seed = 123456789;
 const unsigned int delay_m = 2147483648; // 2³¹
-const unsigned  delay_a = 1103515245;
-const unsigned  delay_c = 12345;
+const unsigned delay_a = 1103515245;
+const unsigned delay_c = 12345;
 const unsigned int delay_max_len = 10000;
 const unsigned int delay_dont_run_prct = delay_m * .01; // %
 
-__attribute__((annotate("NoResistor"))) void seed_write() {
-    // Implement delay_a function to save delay_seed to non-volatile memory
+void seed_write() {
+  // Implement delay_a function to save delay_seed to non-volatile memory
 }
 
-__attribute__((annotate("NoResistor"))) void seed_read() {
-    // Implement delay_a function to read delay_seed from non-volatile memory
+void seed_read() {
+  // Implement delay_a function to read delay_seed from non-volatile memory
 }
 
-__attribute__((annotate("NoResistor"))) void gpdelay() {
-    if (delay_seed == 123456789) {
-        seed_read();
-    }
-    // Update Seed
-    delay_seed = (delay_a * delay_seed + delay_c) % delay_m;
+__attribute__((annotate("NoDelay"))) void gpdelay() {
+  if (delay_seed == 123456789) {
+    seed_read();
+  }
+  // Update Seed
+  delay_seed = (delay_a * delay_seed + delay_c) % delay_m;
 
-    // Don't always execute loop
-    if (delay_seed < delay_dont_run_prct) {
-        // Ensure that only loop delay_a maximum number of times (even delay_a few instructions can throw off delay_a glitch)
-        unsigned int loop_len = delay_seed%delay_max_len;
-        for (int x = 0; x < loop_len; x++) asm("");
-    }
+  // Don't always execute loop
+  if (delay_seed < delay_dont_run_prct) {
+    // Ensure that only loop delay_a maximum number of times (even delay_a few
+    // instructions can throw off delay_a glitch)
+    unsigned int loop_len = delay_seed % delay_max_len;
+    for (int x = 0; x < loop_len; x++)
+      asm("");
+  }
 
-    // Update oudelay_seedr 
-    seed_write();
-
+  // Update oudelay_seedr
+  seed_write();
 }
--- instrumenter/instrumentation_snippets/integritycheckfunc.c
+++ instrumenter/instrumentation_snippets/integritycheckfunc.c
@@ -11,29 +11,23 @@
 // If the integrity is valid then it reads size number of bytes
 // into the memory pointed by the dst pointer.
 // also it returns 1 if the integrity is valid else returns 0.
-__attribute__((annotate("NoResistor"))) int gp_safe_read(void *src, void *src_int, void *dst, unsigned size)
-{
+int gp_safe_read(void *src, void *src_int, void *dst, unsigned size) {
   // read safely from the provided src with verifying integrity.
   // try reading longs.
   int integrityIsFine = TRUE;
-  if (size > 0)
-  {
+  if (size > 0) {
     // first verify the sanity of
     // long number of bytes.
     // We do it in 2 passes.
     // first we read in the multiple of long bytes.
     // next if the remaining bytes are less than long bytes
     // then we ready one byte at a time.
-    while (size > sizeof(long))
-    {
+    while (size > sizeof(long)) {
       long rdVal = *((long *)src);
       long sanVal = *((long *)src_int);
-      if ((rdVal ^ sanVal) == LONGINTEGRITYVALUE)
-      {
+      if ((rdVal ^ sanVal) == LONGINTEGRITYVALUE) {
         *((long *)dst) = rdVal;
-      }
-      else
-      {
+      } else {
         integrityIsFine = FALSE;
         break;
       }
@@ -44,18 +38,13 @@ __attribute__((annotate("NoResistor"))) int gp_safe_read(void *src, void *src_in
     }
 
     // only if the integrity is fine.
-    if (integrityIsFine)
-    {
-      while (size)
-      {
+    if (integrityIsFine) {
+      while (size) {
         char rdVal = *((char *)src);
         char sanVal = *((char *)src_int);
-        if ((rdVal ^ sanVal) == CHARINTEGRITYVALUE)
-        {
+        if ((rdVal ^ sanVal) == CHARINTEGRITYVALUE) {
           *((char *)dst) = rdVal;
-        }
-        else
-        {
+        } else {
           integrityIsFine = 0;
           break;
         }
@@ -72,11 +61,9 @@ __attribute__((annotate("NoResistor"))) int gp_safe_read(void *src, void *src_in
 
 // helper function that reads an integer from the
 // provided address
-__attribute__((annotate("NoResistor"))) int gp_read_int(void *src, void *src_int)
-{
+int gp_read_int(void *src, void *src_int) {
   int to_read = -1;
-  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(int)) != TRUE)
-  {
+  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(int)) != TRUE) {
     // integrity failed.
     // as of
     gr_glitch_detected();
@@ -85,11 +72,9 @@ __attribute__((annotate("NoResistor"))) int gp_read_int(void *src, void *src_int
 }
 
 // helper function that reads a charecter from the provided buffer
-__attribute__((annotate("NoResistor"))) char gp_read_char(void *src, void *src_int)
-{
+char gp_read_char(void *src, void *src_int) {
   char to_read = -1;
-  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(char)) != TRUE)
-  {
+  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(char)) != TRUE) {
     // integrity failed.
     gr_glitch_detected();
   }
@@ -97,11 +82,9 @@ __attribute__((annotate("NoResistor"))) char gp_read_char(void *src, void *src_i
 }
 
 // helper function that reads a pointer value from the provided buffer
-__attribute__((annotate("NoResistor"))) void *gp_read_ptr(void *src, void *src_int)
-{
+void *gp_read_ptr(void *src, void *src_int) {
   void *to_read = NULL;
-  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(void *)) != TRUE)
-  {
+  if (gp_safe_read(src, src_int, (void *)(&to_read), sizeof(void *)) != TRUE) {
     // integrity failed.
     gr_glitch_detected();
   }
@@ -110,17 +93,15 @@ __attribute__((annotate("NoResistor"))) void *gp_read_ptr(void *src, void *src_i
 
 // This function writes size number of bytes into the memory pointed by dst
 // from the memory pointed by the src pointer.
-// It also writes the integrity value into the memory pointed by dst_int pointer.
-__attribute__((annotate("NoResistor"))) void gp_safe_write(void *dst, void *dst_int, void *src, unsigned size)
-{
-  if (size > 0)
-  {
+// It also writes the integrity value into the memory pointed by dst_int
+// pointer.
+void gp_safe_write(void *dst, void *dst_int, void *src, unsigned size) {
+  if (size > 0) {
     // We do it in 2 passes.
     // first we write in the multiple of long bytes.
     // next if the remaining bytes are less than long bytes
     // then we write one byte at a time.
-    while (size > sizeof(long))
-    {
+    while (size > sizeof(long)) {
       long wrVal = *((long *)src);
       long wrIntVal = wrVal ^ LONGINTEGRITYVALUE;
       *((long *)dst) = wrVal;
@@ -131,8 +112,7 @@ __attribute__((annotate("NoResistor"))) void gp_safe_write(void *dst, void *dst_
       src += sizeof(long);
       size -= sizeof(long);
     }
-    while (size)
-    {
+    while (size) {
       char wrVal = *((char *)src);
       char wrIntVal = wrVal ^ CHARINTEGRITYVALUE;
       *((char *)dst) = wrVal;
@@ -148,19 +128,16 @@ __attribute__((annotate("NoResistor"))) void gp_safe_write(void *dst, void *dst_
 
 // helper function that writes an integer from the
 // provided address
-__attribute__((annotate("NoResistor"))) void gp_write_int(void *dst, void *dst_int, int toWrite)
-{
+void gp_write_int(void *dst, void *dst_int, int toWrite) {
   gp_safe_write(dst, dst_int, (void *)(&toWrite), sizeof(int));
 }
 
 // helper function that writes a character from the provided buffer
-__attribute__((annotate("NoResistor"))) void gp_write_char(void *dst, void *dst_int, char toWrite)
-{
+void gp_write_char(void *dst, void *dst_int, char toWrite) {
   gp_safe_write(dst, dst_int, (void *)(&toWrite), sizeof(char));
 }
 
 // helper function that writes a pointer value into the provided buffer
-__attribute__((annotate("NoResistor"))) void gp_write_ptr(void *dst, void *dst_int, void *toWrite)
-{
+void gp_write_ptr(void *dst, void *dst_int, void *toWrite) {
   gp_safe_write(dst, dst_int, (void *)(&toWrite), sizeof(void *));
 }
