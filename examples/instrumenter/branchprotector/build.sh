d744300129efeb7ef7e26dc3f9eb8e5b28e3d9e6
Chad Spensky <cspensky@gmail.com>
2019-09-03 10:15:10 -0400

    Switching CC to clang

--- examples/instrumenter/branchprotector/build.sh
+++ examples/instrumenter/branchprotector/build.sh
@@ -3,7 +3,7 @@
 ../../../build_passes.sh
 
 export LLVM_COMPILER=clang
-export CC=wllvm
+export CC=clang
 export CFLAGS="-Xclang -load -Xclang ../../../instrumenter/build/BranchProtector/libBranchProtector.so"
 make clean
 make
