ac96b3bf0d98808262e9cdac7c1d3bb78495c48d
Chad Spensky <cspensky@gmail.com>
2019-09-05 14:00:12 -0400

    removing unused firmware

--- firmware/stm32l152re/simple/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-../../../build_passes.sh
-  
-export LLVM_COMPILER=clang
-export CLANG_FLAGS+="-Xclang -load -Xclang ../../../instrumenter/build/DelayInjector/libDelayInjector.so "
-export CLANG_FLAGS+="-Xclang -load -Xclang ../../../instrumenter/build/BranchProtector/libBranchProtector.so "
-CC=clang make
